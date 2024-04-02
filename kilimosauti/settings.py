42449c9f17a327eb2d28cf7d5e2d5b519eb98a7a
Hassanyange <hassanyange@gmail.com>
2024-04-02 14:21:58 +0300

    some changes

--- kilimosauti/settings.py
+++ kilimosauti/settings.py
@@ -119,16 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_URL = '/static/'  # Add a leading slash
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
